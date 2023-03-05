6dfa5f1696e5cfa79ff16d9aacd07a2e2da800f1
Jay Boricha <jayboricha707@gmail.com>
2023-03-05 15:23:41 +0530

    last test case remaining in drum-machine

--- Drum-machine/src/App.jsx
+++ Drum-machine/src/App.jsx
@@ -1,6 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
-import { useState, useEffect } from "react";
-import "./App.css";
 
 const audioClips = [
   {
@@ -42,7 +41,7 @@ const audioClips = [
   {
     keyCode: 90,
     keyTrigger: "Z",
-    id: "Kick-n'-Hat",
+    id: "Hat",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
   },
   {
@@ -54,76 +53,120 @@ const audioClips = [
   {
     keyCode: 67,
     keyTrigger: "C",
-    id: "Closed-HH",
+    id: "Closed-jj",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
   },
-];
-const audio = new Audio();
-function App() {
-  const [volume, setVolume] = useState(1);
+]
+
+function App(){
+  const [volume , setVolume] = React.useState(1);
+  const [recording , setRecording] = React.useState("");
+  const [speed, setSpeed] = React.useState(0.5);
+
+  const playRecording = () => {
+    let index = 0;
+    let recordArray = recording.split("");
+    const interval = setInterval(() => {
+      const audioTag = document.getElementById(recordArray[index]);
+      audioTag.volume = volume;
+      audioTag.currentTime = 0;
+      audioTag.play();
+      index++;
+    }, speed * 600);
+    setTimeout(
+      () => clearInterval(interval),
+      600 * speed * recordArray.length - 1
+    );
+  };
+
+
   return (
-    <div className="bg-info min-vh-100 text-white" id="drum-machine">
-      <div className="text-center">
-        <h2>Drum-Machine</h2>
-        {audioClips.map((clip) => (
-          <Pad key={clip.id} clip={clip} volume={volume} />
-        ))}
+    <div id="drum-machine" className="text-center bg-info w-100 min-vh-100 text-white">
+        <h2>Drum Machine</h2>
+        <div className="w-30">
+        {
+          audioClips.map(clip => (
+            <Pad 
+              key = {clip.id}
+              clip = {clip}
+              volume = {volume}
+              setRecording={setRecording}
+            />
+          ))
+        }
+        </div>
         <br />
         <h4>Volume</h4>
-        <input
-          type="range"
-          step="0.01"
+        <input 
+          id="display"
+          type={"range"}
+          step ="0.01"
           onChange={(e) => setVolume(e.target.value)}
-          volume={volume}
+          value={volume}
           max="1"
           min="0"
           className="w-50"
-        />
-      </div>
+          />
+          <h3>{recording}</h3>
+          {
+            (
+            <>
+              <button onClick={playRecording} className="btn btn-success m-4">Play</button>
+              <button onClick={() => setRecording("")} className="btn btn-danger m-4">Clear</button>
+              <br />
+              <h4>Speed</h4>
+              <input 
+                type="range"
+                step={0.01}
+                onChange={(e) => setSpeed(e.target.value)}
+                value={speed}
+                max="1.2"
+                min={0.1}
+                className="w-50"
+              />
+            </>
+          )}
     </div>
-  );
+  )
 }
 
-function Pad({ clip , volume}) {
-  useEffect(() => {
+function Pad({clip , volume , setRecording}) {
+  const [active , setActive] = React.useState(false);
+
+  React.useEffect(() => {
     document.addEventListener("keydown", handleKeyPress);
     return () => {
       document.removeEventListener("keydown", handleKeyPress);
     };
   }, []);
-  const handleKeyPress = (e) => {
-    // console.log(e.keyCode)
-    // console.log(clip.keyCode)
-    // if(e.keyCode === clip.keyCode){
-    //     playSound()
-    // }
-    audioClips.forEach((clip) => {
-      if (e.keyCode === clip.keyCode) {
-        let playUrl = clip.url;
-        playOnKeyPress(playUrl);
-      }
-    });
-  };
 
-  const playOnKeyPress = (url) => {
-    audio.src = url;
-    audio.play();
-    audio.volume = volume;
+  const handleKeyPress = (e) => {
+    if(e.keyCode === clip.keyCode){
+      playSound();
+    }
   };
 
   const playSound = () => {
     const audioTag = document.getElementById(clip.keyTrigger);
+    setActive(true);
+    setTimeout(() => setActive(false), 200);
+    audioTag.volume = volume;
     audioTag.currentTime = 0;
     audioTag.play();
-    audioTag.volume = volume;
+    setRecording((prev) => prev + clip.keyTrigger + " ");
   };
 
   return (
-    <div onClick={playSound} className={`btn btn-secondary p-4 m-3 drum-pad`} id="display">
+    <div
+      id="#1"
+      onClick={playSound}
+      className={`drum-pad btn btn-secondary p-4 m-3 ${active && "btn-warning"}`}
+    >
       <audio className="clip" id={clip.keyTrigger} src={clip.url} />
       {clip.keyTrigger}
     </div>
-  );
+  )
+  
 }
 
 export default App;
